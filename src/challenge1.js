6fc08c7d884b676c007dc0cf6b8cdacf17539483
ZDaraoui <zdaraoui2@gmail.com>
2021-03-04 10:37:03 +0000

    Challenge 1 Solution

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,2 +1,11 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
+
+//function 
+function greet() {
+
+//return the string
+    return "Hi, Ed!"
+}
+// console log
+console.log(greet());
