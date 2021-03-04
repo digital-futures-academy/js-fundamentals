4e7eb21fcb4028376433699123959481f47f3608
Matthew Fanning <mfcareers@yahoo.com>
2021-03-04 12:27:01 +0000

    commit 1 (proper)

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,6 +1,8 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
 
-funct greet(){
-    return console.log('Hi, Ed!')
+function greet(){
+    console.log('Hi, Ed!')
 }
+
+greet()
