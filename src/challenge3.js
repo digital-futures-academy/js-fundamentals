f1281c73d9768e1c934d9bac2de80554cb158111
R B <ritthikiit@gmail.com>
2021-03-04 11:09:20 +0000

    Challenge3

---
+++ src/challenge3.js
@@ -0,0 +1,8 @@
+// Using the function keyword declare a function called `greet`
+// that returns a String "Hi, Ed!"
+
+const  greet = (name) => {
+    return `Hi, ${name}!`
+}
+
+console.log(greet("Ritthik and Josh"))
