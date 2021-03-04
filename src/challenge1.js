b616501887c14549f373d7ac5be37e9131f5e3d1
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-03-04 10:36:10 +0000

    challenge 1

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,2 +1,9 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
+
+function greet() {
+    return "Hi, Ed!";
+}
+
+let answer = greet();
+console.log(answer);
