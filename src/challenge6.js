01c5cd68480396781abfe8b7477618202ec1fc08
Aydan Awan <aydan.awan@outlook.com>
2021-03-04 16:03:47 +0000

    challenge 6

---
+++ src/challenge6.js
@@ -0,0 +1,19 @@
+const alphabet = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*()-_=+[]{};:\'"\\|,.<>/?`~§±1234567890'
+//console.log(alphabet.length)
+console.log(alphabet.indexOf(' '))
+
+
+function convert(string){
+
+let ans = ''
+for (i = 0; i < string.length; i++) {
+    
+    ans += (alphabet.indexOf(string[i])+1)
+
+}
+console.log(ans)
+
+}
+
+convert('Hi, Ed!')
+
