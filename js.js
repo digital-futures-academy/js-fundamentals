10c156d502049982ccc8e160f58ffa7864f3f912
Lewis Bensted <lewisbensted@hotmail.com>
2021-03-09 18:02:07 +0000

    updated classes for bank.js

---
+++ js.js
@@ -0,0 +1,5 @@
+function hello(x){x=x+3}
+
+let y=3
+hello(y)
+console.log(y)
--- src/bank.js
+++ src/bank.js
@@ -1,46 +1,56 @@
-class AccountInformation{
-    constructor(balance){
-        this.balance=balance
-        this.history=[]
-    }
-    statement() {
+class AccountStatement{
+    statement(history) {
         console.log('date || credit || debit || balance')
-        for (let item of this.history) {
+        for (let item of history) {
             console.log(`${item[0]}|| ${item[1]} ||${item[2]}|| ${item[3]}`)
         }
     }
 }
 
-class AccountActions {
-    constructor(balance){
-        this.accInfo=new AccountInformation(balance)
-    }
-
-    deposit(amount){
-        let date=new Date()
-        this.accInfo.balance+=amount
-        this.accInfo.history.unshift([`${date.getDate()}/${date.getMonth()}/${date.getYear()}`,amount,null,this.accInfo.balance])
-        console.log(`£${amount} deposited`)
-        }
-
-
-    withdraw(amount){
+class AccountActions{
+    deposit(amount, balance, history){
         let date= new Date()
-        this.accInfo.balance-=amount
-        this.accInfo.history.unshift([`${date.getDate()}/${date.getMonth()}/${date.getYear()}`,null,amount,this.accInfo.balance])
-        console.log(`£${amount} withdrawn`)}
-
-
-    statement(){
-        this.accInfo.statement()
+        balance+=amount
+        history.unshift([`${date.getDate()}/${date.getMonth()}/${date.getYear()}`,amount,null,balance])
+        return [balance, history]
     }
-
+    withdraw(amount, balance, history){
+        let date=new Date()
+        balance-=amount
+        history.unshift([`${date.getDate()}/${date.getMonth()}/${date.getYear()}`,null,amount,balance])
+        return [balance, history]
+    }
 }
 
 
-let acc=new AccountActions(100)
-acc.deposit(50)
-acc.withdraw(10)
-acc.withdraw(25)
-acc.deposit(20)
+
+class Account{
+    constructor (balance=0, history=[]){
+        this.balance=balance
+        this.history=history
+        this.accStatement=new AccountStatement()
+        this.accActions=new AccountActions()
+    }
+    statement(){
+        this.accStatement.statement(this.history)
+    }
+    deposit(amount){
+        let result=this.accActions.deposit(amount, this.balance, this.history)
+        this.balance=result[0]
+        this.history=result[1]
+    }
+    withdraw(amount){
+        let result = this.balance=this.accActions.withdraw(amount, this.balance, this.history)
+        this.balance=result[0]
+        this.history=result[1]
+    }
+}
+
+let acc=new Account(100)
+
+acc.deposit(40)
+acc.statement()
+acc.withdraw(20)
+acc.statement()
+acc.deposit(60)
 acc.statement()
