879e59643f87b7151a6d5f8c1ca8514fc4992f50
Colin <cln.gardiner@outlook.com>
2021-06-22 14:17:35 +0100

    completed challenge 1

---
+++ src/challenge1.js
@@ -0,0 +1,9 @@
+// Using the function keyword declare a function called `greet`
+// that returns a String "Hi, Ed!"
+
+function greet () {
+    return "Hi, Ed!"
+}
+
+//Output: "Hi, Ed"
+console.log("Hi, Ed!")
