9ad09b92887231ef9b85555ef56ae45e5dcd1adc
Rawdah Chowdhury <rawdah98@outlook.com>
2021-06-23 12:30:54 +0100

    Challenge 5 completed

--- src/challenge5.js
+++ src/challenge5.js
@@ -1,9 +1,4 @@
-//Use class syntax to create a Student class that initializes with a 
-//name property and has a method that returns the capitalised name.
 
-//```js
-//let student = new Student("asia")
-//student.capitalisedName() // "Asia"
 
 class Student {
 constructor (name){
