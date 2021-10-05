8b991dcd8de2ca0dfdbfef9b91233f3123f7c64b
Nikesh Asokcumar <nikeshasokcumar@hotmail.com>
2021-10-05 11:31:29 +0100

    "removed a line"

--- src/challenge5.js
+++ src/challenge5.js
@@ -3,8 +3,7 @@ class Student {
         this.name = name;
     }
     capitalisedName() {
-        const editName = this.name[0].toUpperCase() + this.name.slice(1);
-        return editName;
+        return this.name[0].toUpperCase() + this.name.slice(1);
     }
 };
 
