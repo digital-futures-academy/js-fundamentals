3a1212763803b1d1709aa74b77c3b989b0c520c4
hamsa-mo <91551907+hamsa-mo@users.noreply.github.com>
2021-10-10 21:26:22 +0100

    Delete challenge5.js

--- challenge5.js
+++
@@ -1,9 +0,0 @@
-class Student {
-    constructor(name) {
-        this.name = name;
-    }
-    capitalisedName() {
-        return this.name.charAt(0).toUpperCase() + this.name.slice(1);
-    }
-}
-
