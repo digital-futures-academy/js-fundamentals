34df8191fa68cf82f2f77f0d5f5577edcc04e390
Matt <wyattmatt@protonmail.com>
2021-12-29 15:53:50 +0000

    Challenge 6 completed! Challenge 7 started

---
+++ src/challenge7.js
@@ -0,0 +1,8 @@
+function encrypt(plaintext, key) {
+    // A function to encrpyt plain text based on substitution (given table of values). A key is required to indicate offset of substitutions.
+}
+
+function LetterNumber() {
+    // This is a class.
+    this.encrypt = encrypt;
+}
