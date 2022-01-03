6e0a6cf369eb020c39afdf2317a30f367211109e
RSmith-TD <rogan.smith@hotmail.co.uk>
2022-01-03 19:40:50 +0000

    challenge5 Complete

---
+++ src/challenge5.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+    capitalisedName() {
+        return this.name[0].toUpperCase() + this.name.slice(1);
+    }
+}
+
+// let student = new Student("asia")
+// student.capitalisedName() // "Asia"
