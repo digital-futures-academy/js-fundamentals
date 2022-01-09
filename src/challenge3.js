e5205bca04177aea4b2da72f79157f292e052ac8
Daisy Lee <daisymcylee@gmail.com>
2022-01-09 21:24:23 +0000

    Completed Challenge 3

--- src/challenge3.js
+++ src/challenge3.js
@@ -1,1 +1,7 @@
+// Declare a function called greet that has 1 argument called name and returns a String "Hi, <name>!" - using ARROW NOTATION 
 
+const greet = name => {
+    return `Hi, ${name}!`
+}
+
+console.log(greet('Ben'));
