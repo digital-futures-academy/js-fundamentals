1d8ef701900fbadaa719e1c15884abfa4ed138f8
Daisy Lee <daisymcylee@gmail.com>
2022-01-09 21:12:28 +0000

    Completed Challenge 2

--- src/challenge2.js
+++ src/challenge2.js
@@ -1,1 +1,7 @@
+//Declare a function called greet that has 1 argument called name and returns a String "Hi, <name>!"
 
+function greet(name) {
+    return `Hi, ${name}!`
+};
+
+console.log(greet('Daisy'));
