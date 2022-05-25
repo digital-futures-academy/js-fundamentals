bf901cf675dff0b5f32093e25a79960b05f8de69
RoxanaTurc <94209708+RoxanaTurc@users.noreply.github.com>
2022-05-25 19:41:52 +0100

    Create challenge1.js

---
+++ challenges/challenge1.js
@@ -0,0 +1,9 @@
+#### Requirements
+Using the `function` keyword to declare a function called `greet` that returns a String `"Hi, Ed!"`
+
+//solution to the challenge 1 below
+function greet() {
+	console.log('Hi,Ed!') 
+}
+	greet();
+	
