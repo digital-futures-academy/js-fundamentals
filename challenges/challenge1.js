b8a8f9f20853a7acac20b9f75e06434bdda8b01d
RoxanaTurc <94209708+RoxanaTurc@users.noreply.github.com>
2022-05-28 14:33:25 +0100

    Delete challenge1.js
    removed file from incorrect path

--- challenges/challenge1.js
+++
@@ -1,9 +0,0 @@
-#### Requirements
-Using the `function` keyword to declare a function called `greet` that returns a String `"Hi, Ed!"`
-
-//solution to the challenge 1 below
-function greet() {
-	console.log('Hi,Ed!') 
-}
-	greet();
-	
