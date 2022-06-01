c99d2e57b497246200e1c54fe7a51c1bf3c43559
Shafiul23 <106149305+Shafiul23@users.noreply.github.com>
2022-06-01 17:57:05 +0100

    challenge 1 complete

---
+++ challenges/challenges.js
@@ -0,0 +1,13 @@
+//challenge1
+function greet() {
+    console.log('Hi!, Ed!');
+}
+
+greet()
+
+//challenge 2
+function greet(name) {
+    return `Hi, ${name}`;
+}
+console.log('test')
+greet(name);
