f42070fc31910d1b1326fc0df062d75738c81623
Shafiul23 <106149305+Shafiul23@users.noreply.github.com>
2022-06-02 10:31:15 +0100

    challenge 1 and 2, src folder

---
+++ src/challenge2.js
@@ -0,0 +1,32 @@
+/*#### Requirements
+Declare a `function` called `greet` that has 1 argument called `name` and returns a String`"Hi, <name>!"` */
+
+/*
+const greet(name) {
+    return `Hi, ${name}`;
+}
+console.log('test')
+
+greet(name);
+
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+let name = 'Shaf';
+greet(name)
+
+const name = function greet() {
+    return `Hello, ${name}!`;
+}
+
+greet('shaf')
+*/
+
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log(greet("Shaf"))
+
