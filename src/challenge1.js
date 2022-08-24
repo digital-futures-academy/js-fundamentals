c3b35f6bff747b824c5ceb94c88cf404fff28f63
Deryn Cullen <info@deryncullen.com>
2022-08-24 14:14:23 +0100

    Challenge1 complete - D Cullen

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,2 +1,8 @@
+"use strict";
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
+function greet() {
+  const hello = "Hi, Ed!";
+  return hello;
+}
+greet();
