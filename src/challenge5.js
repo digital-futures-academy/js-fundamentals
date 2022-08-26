459bb556c831aaac2db23d8496db16ef929d3dfd
Deryn <106682214+derynL@users.noreply.github.com>
2022-08-26 14:31:22 +0100

    Challenge5 complete

---
+++ src/challenge5.js
@@ -0,0 +1,12 @@
+'use strict';
+class Student {
+  constructor(name) {
+    this.name = name;
+  }
+  capitalisedName() {
+    return `${this.name.charAt(0).toUpperCase()}${this.name.slice(1)}`;
+  }
+}
+
+let student = new Student('asia');
+console.log(student.capitalisedName(student));
