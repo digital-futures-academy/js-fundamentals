33ef9a75b16009a131ef132a0da025dcacd88f46
Craig Simbarashe Machiya <simba-craig@hotmail.com>
2022-09-15 02:32:08 +0100

    update

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,5 +1,5 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
-function greet(name){
-    console.log(`Hi, ${name}!) 
+function greet(){
+    console.log("Hi, Ed!") 
 }
