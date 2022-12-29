bad21e3dbee7865696d02c6e45c5540f035a3b65
Francisco Bejarano <bejarano.fco@gmail.com>
2022-12-29 02:09:23 +0000

    Challenge6 completed

---
+++ src/challenge6.js
@@ -0,0 +1,305 @@
+const encrypt = (str) => { 
+
+    let encryptedConst = "";
+
+    for(var i = 0; i <= str.length; i++){
+        switch(str.charAt(i)){
+            case " ":
+                encryptedConst += "1";
+                break;
+            case "a":
+                encryptedConst += "2";
+                break;
+            case "b":
+                encryptedConst += "3";
+                break;
+            case "c":
+                encryptedConst += "4";
+                break;
+            case "d":
+                encryptedConst += "5";
+                break;
+            case "e":
+                encryptedConst += "6";
+                break;
+            case "f":
+                encryptedConst += "7";
+                break;
+            case "g":
+                encryptedConst += "8";
+                break;
+            case "h":
+                encryptedConst += "9";
+                break;
+            case "i":
+                encryptedConst += "10";
+                break;
+            case "j": 
+                encryptedConst += "11";
+                break;
+            case "k":
+                encryptedConst += "12";
+                break;
+            case "l":
+                encryptedConst += "13";
+                break;
+            case "m":
+                encryptedConst += "14";
+                break;
+            case "n":
+                encryptedConst += "15";
+                break;
+            case "o":
+                encryptedConst += "16";
+                break;
+            case "p":
+                encryptedConst += "17";
+                break;
+            case "q":
+                encryptedConst += "18";
+                break;
+            case "r":
+                encryptedConst += "19";
+                break;
+            case "s":
+                encryptedConst += "20";
+                break;
+            case "t":
+                encryptedConst += "21";
+                break;
+            case "u":
+                encryptedConst += "22";
+                break;
+            case "v":
+                encryptedConst += "23";
+                break;
+            case "w":
+                encryptedConst += "24";
+                break;
+            case "x":
+                encryptedConst += "25";
+                break;
+            case "y":
+                encryptedConst += "26";
+                break;
+            case "z":
+                encryptedConst += "27";
+                break;
+            case "A":
+                encryptedConst += "28";
+                break;
+            case "B": 
+                encryptedConst += "29";
+                break;
+            case "C":
+                encryptedConst += "30";
+                break;
+            case "D": 
+                encryptedConst += "31";
+                break;
+            case "E": 
+                encryptedConst += "32";
+                break;
+            case "F":
+                encryptedConst += "33";
+                break;
+            case "G":
+                encryptedConst += "34";
+                break;
+            case "H": 
+                encryptedConst += "35";
+                break;
+            case "I":
+                encryptedConst += "36";
+                break;
+            case "J":
+                encryptedConst += "37";
+                break;
+            case "K":
+                encryptedConst += "38";
+                break;
+            case "L":
+                encryptedConst += "39";
+                break;
+            case "M":
+                encryptedConst += "40";
+                break;
+            case "N":
+                encryptedConst += "41";
+                break;
+            case "O":
+                encryptedConst += "42";
+                break;
+            case "P":
+                encryptedConst += "43";
+                break;
+            case "Q":
+                encryptedConst += "44";
+                break;
+            case "R":
+                encryptedConst += "45";
+                break;
+            case "S":
+                encryptedConst += "46";
+                break;
+            case "T":
+                encryptedConst += "47";
+                break;
+            case "U":
+                encryptedConst += "48";
+                break;
+            case "V":
+                encryptedConst += "49";
+                break;
+            case "W":
+                encryptedConst += "50";
+                break;
+            case "X":
+                encryptedConst += "51";
+                break;
+            case "Y":
+                encryptedConst += "52";
+                break;
+            case "Z":
+                encryptedConst += "53";
+                break;
+            case "!":
+                encryptedConst += "54";
+                break;
+            case "@":
+                encryptedConst += "55";
+                break;
+            case "£":
+                encryptedConst += "56";
+                break;
+            case "$":
+                encryptedConst += "57";
+                break;
+            case "%":
+                encryptedConst += "58";
+                break;
+            case "^":
+                encryptedConst += "59";
+                break;
+            case "&":
+                encryptedConst += "60";
+                break;
+            case "*":
+                encryptedConst += "61";
+                break;
+            case "(":
+                encryptedConst += "62";
+                break;
+            case ")":
+                encryptedConst += "63";
+                break;
+            case "-":
+                encryptedConst += "64";
+                break;
+            case "_":
+                encryptedConst += "65";
+                break;
+            case "=":
+                encryptedConst += "66";
+                break;
+            case "+":
+                encryptedConst += "67";
+                break;
+            case "[":
+                encryptedConst += "68";
+                break;
+            case "]":
+                encryptedConst += "69";
+                break;
+            case "{":
+                encryptedConst += "70";
+                break;
+            case "}":
+                encryptedConst += "71";
+                break;
+            case ";":
+                encryptedConst += "72";
+                break;
+            case ":":
+                encryptedConst += "73";
+                break;
+            case "'":
+                encryptedConst += "74";
+                break;
+            case "\"":
+                encryptedConst += "75";
+                break;
+            case "\\":
+                encryptedConst += "76";
+                break;
+            case "|":
+                encryptedConst += "77";
+                break;
+            case ",":
+                encryptedConst += "78";
+                break;
+            case ".":
+                encryptedConst += "79";
+                break;
+            case "<":
+                encryptedConst += "80";
+                break;
+            case ">":
+                encryptedConst += "81";
+                break;
+            case "/":
+                encryptedConst += "82";
+                break;
+            case "?":
+                encryptedConst += "83";
+                break;
+            case "\`":
+                encryptedConst += "84";
+                break;
+            case "~":
+                encryptedConst += "85";
+                break;
+            case "§":
+                encryptedConst += "86";
+                break;
+            case "±":
+                encryptedConst += "87";
+                break;
+            case "1":
+                encryptedConst += "88";
+                break;
+            case "2":
+                encryptedConst += "89";
+                break;
+            case "3":
+                encryptedConst += "90";
+                break;
+            case "4":
+                
+            break;encryptedConst += "91";
+            case "5":
+                encryptedConst += "92";
+                break;
+            case "6":
+                encryptedConst += "93";
+                break;
+            case "7":
+                encryptedConst += "94";
+                break;
+            case "8":
+                encryptedConst += "95";
+                break;
+            case "9":
+                encryptedConst += "96";
+                break;
+            case "0":
+                encryptedConst += "97"
+                break;
+
+        }       
+    }
+
+    return encryptedConst;
+}
+
+console.log(encrypt("Hi, Ed!"));
