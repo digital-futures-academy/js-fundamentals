280c395c8c597bfa25f822c48e241cde29c55b8e
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-05-01 16:15:09 +0100

    challenge 3 completed, tested 1-3

--- src/challenge1.js
+++ src/challenge1.js
@@ -3,3 +3,5 @@
 function greet() {
     return 'Hi, Ed!';
 }
+
+//console.log(greet());
--- src/challenge3.js
+++ src/challenge3.js
@@ -1,6 +1,6 @@
 // Declare a `function` called `greet` that has 1 argument called `name` and returns a String `"Hi, <name>!"`
-function greet(name) {
-    return `Hi, ${name}`;
-}
+// Same as Challenge 2 - this time use an arrow function
+
+const greet = name => `Hi, ${name}`;
 
 //console.log(greet('Wilfrick'));
