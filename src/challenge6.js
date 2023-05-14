bbc75ecbcb686dd1c64a674926abfc46f5843a1e
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-14 21:12:28 +0100

    refactored and implemented challenge6

--- src/challenge6.js
+++ src/challenge6.js
@@ -1,19 +1,30 @@
+import("node-fetch");
+
 const url = 'https://gist.githubusercontent.com/dearshrewdwit/691c71616995ad2430ab309aa9998745/raw/78624a6c9499e049fd294ddb8302d93e6f675a0d/character_set.txt'
-const fetchPromise = import("node-fetch");
+const substitutionDict = {};
 
-fetchPromise
-  .then(({ default: fetch }) => {
-    fetch(url)
-        .then(response => response.text())
-        .then(tableOfValues => {
-            const substitutionDict = {};
+async function loadSubstitutionDict() {
+    const response = await fetch(url);
+    const tableOfValues = await response.text();
+    tableOfValues.split("\n").forEach(line => {
+        const [key, value] = line.split(', ');
+        substitutionDict[key] = value;
+    });
+}
 
-            tableOfValues.split("\n").forEach(line => {
-                const [key, value] = line.split(', ');
-                substitutionDict[key] = value;
-            });
+function encript(string) {
+    encripted_string = '';
+    characters = string.split('');
+    characters.forEach(character => {
+        encripted_string += substitutionDict[character];
+    });
+    return encripted_string;
+}
 
-            console.log(substitutionDict);
-        });
-  })
-  .catch((error) => console.log(error));
+loadSubstitutionDict()
+    .then(() => {
+        console.log(encript('E'));
+        console.log(encript('Ed'));
+        console.log(encript('Ed'));
+        console.log(encript('Hi, Ed!'));
+    });
