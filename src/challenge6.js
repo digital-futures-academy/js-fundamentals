991a781aa9b46974a156d7a8773e02e460c49694
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-14 22:01:39 +0100

    refactored: created a separate file for load() function

--- src/challenge6.js
+++ src/challenge6.js
@@ -1,18 +1,8 @@
-import("node-fetch");
+// load() function was initially in this file, 
+// I refactored it in a new file when implementing challenge7
+const substitutionDictLoader = require("./substitutionDictLoader");
 
-const url = 'https://gist.githubusercontent.com/dearshrewdwit/691c71616995ad2430ab309aa9998745/raw/78624a6c9499e049fd294ddb8302d93e6f675a0d/character_set.txt'
-const substitutionDict = {};
-
-async function loadSubstitutionDict() {
-    const response = await fetch(url);
-    const tableOfValues = await response.text();
-    tableOfValues.split("\n").forEach(line => {
-        const [key, value] = line.split(', ');
-        substitutionDict[key] = value;
-    });
-}
-
-function encript(string) {
+function encript(string, substitutionDict) {
     encripted_string = '';
     characters = string.split('');
     characters.forEach(character => {
@@ -21,10 +11,10 @@ function encript(string) {
     return encripted_string;
 }
 
-loadSubstitutionDict()
-    .then(() => {
-        console.log(encript('E'));
-        console.log(encript('Ed'));
-        console.log(encript('Ed'));
-        console.log(encript('Hi, Ed!'));
+substitutionDictLoader.load()
+    .then((result) => {
+        console.log(encript('E', result));
+        console.log(encript('Ed', result));
+        console.log(encript('Ed', result));
+        console.log(encript('Hi, Ed!', result));
     });
