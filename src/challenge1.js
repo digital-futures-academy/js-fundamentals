dba259b6b8fe76afbaef2bc29dd4e41ecbb96bff
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-05-17 01:07:42 +0300

    Challenge 1 complete

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,2 +1,7 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
+function greet(greeting) {
+    return greeting
+}
+
+console.log(greet("Hi, Ed!"))
