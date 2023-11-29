9f8084f2d4e5e17938526ad033d0a29b9ff6f5ca
SKAntoniou <SKAntoniou@gmail.com>
2023-11-29 22:06:12 +0000

    fixed typo

--- src/challenge3.js
+++ src/challenge3.js
@@ -1,4 +1,4 @@
-// As I already did an arrow function for challege 2, I will do a normal function for challege 3
+// As I already did an arrow function for challenge 2, I will do a normal function for challenge 3
 function greet(name) {
     return `"Hi, ${name}"`
 }
