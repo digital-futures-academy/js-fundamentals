5654d20a55c7e809e3370adac3a227608dc586ef
chris-mark-wilson <cmwilson.co.uk>
2023-12-08 16:23:37 +0000

    pre npm challenge11 solution complete and refactored

--- src/challenge11.js
+++ src/challenge11.js
@@ -6,6 +6,7 @@ class Cipher {
         this.charSet = charSet;//cipher file
        
     }
+   
 
     async buildLookup(key) {
         try {
@@ -21,8 +22,8 @@ class Cipher {
                 ?array[(index+key)%array.length].split(', ')[1].toString().padStart(2,'0')
                 :array[(index+key)%array.length].split(', ')[1];
             })
-            console.log(lookup);
-            fs.writeFile('./data/lookup.json', JSON.stringify(lookup,null,4));
+          
+          
             return lookup;
         }
         catch (err) {
@@ -32,26 +33,31 @@ class Cipher {
         }
     }
     async encode(fileName,key) {
-        if (!fileName.endsWith('.txt')) { 
-            console.log('File must have a .txt extension'); return; }
+         console.log(`encoding ${fileName} using key ${key}`);
         try {
             const lookup = await this.buildLookup(key);
+
             const data = await fs.readFile(fileName, 'utf-8');
             const array = data.split('');
             const result = array.map(char => lookup[char]).join('');
             await fs.writeFile(`${fileName}.enc`, result);
             console.log(`File successfully encoded as ${fileName}.enc`);
+            return;
         } catch (err) {
             console.log(err);
             return;
         }
     }
 
-    async decode(fileName,key) {
-        if (!fileName.endsWith('.txt.enc')) { 
-            console.log('File must have a .txt.enc extension'); return; }
+    async decode(fileName, key) {
+      console.log(`decoding ${fileName} using key ${key}`)
         try {
             const lookup = await this.buildLookup(key);
+             if (Object.values(lookup).length != 97) {
+               console.log("Charset must contain 97 characters");
+
+               return;
+             }
             const data = await fs.readFile(fileName, 'utf-8');
           
             let array = data.split('');
@@ -68,7 +74,8 @@ class Cipher {
                 }
             }).join('');
             await fs.writeFile(`${fileName.substring(0,fileName.indexOf(".enc"))}`, result);
-            console.log(`File successfully decoded as ${fileName.substring(0,fileName.indexOf(".enc"))}`);
+            console.log(`File successfully decoded as ${fileName.substring(0, fileName.indexOf(".enc"))}`);
+            return;
         } catch (err) {
             console.log(err);
             return;
@@ -79,12 +86,15 @@ class Cipher {
 //usage - create new instance of Cipher class
 //e.g. const myCipher = new Cipher(charSet, fileName, key);
 
-const numCipher = new Cipher(`./data/charNum.txt`);
-numCipher.encode('../example.txt', 10005)
-.then(() => numCipher.decode('../example.txt.enc', 10005)
-)
+// const numCipher = new Cipher(`./data/charNum.txt`);
+// numCipher
+//   .encode("../example.txt", 10005)
+//   .then(() => numCipher.decode("../example.txt.enc", 10005))
+//   .then(() => {
+//     const charCipher = new Cipher(`./data/charChar.txt`);
+//     charCipher
+//       .encode("../example.txt", 10096)
+//       .then(() => charCipher.decode("../example.txt.enc", 10096));
+//   });
 
-const charCipher=new Cipher(`./data/charChar.txt`);
-charCipher.encode('../example.txt', 10096)
-.then(() => charCipher.decode('../example.txt.enc', 10096)
-)
+module.exports = Cipher;
