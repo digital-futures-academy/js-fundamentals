115250be3917a873031479b26e764baca1cf3b27
Eliott McKenzie <e4991m@gmail.com>
2024-01-09 23:30:05 +0000

    finished challenge 5

--- src/challenge5.js
+++ src/challenge5.js
@@ -7,11 +7,12 @@ class Student {
     this.name = name
   }
   capitalisedName() {
-    
+    return 'Result:', this.name.charAt(0).toUpperCase() + this.name.slice(1)
   }
 }
 
 let student = new Student("asia")
 student.capitalisedName() // "Asia"
+// console.log(student)
 
 
