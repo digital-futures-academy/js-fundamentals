ee0baeaf9ef042c2cb943ff22264bdae1f818663
Noman Jafri <snomanh@hotmail.com>
2024-02-22 01:24:48 +0000

    challenge 3 complete

---
+++ src/challenge3.js
@@ -0,0 +1,8 @@
+/*Requirements
+Declare a `function` called `greet` that has 1 argument 
+called `name` and returns a String `"Hi, <name>!"`
+Same as Challenge 2 - this time use an arrow function */
+
+const greet = name => `Hi, ${name}!`;
+
+console.log(greet("Ed"));
