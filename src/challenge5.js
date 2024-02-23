0549ae649709dc95554228aa0975a333034a4038
JaviRute <jrute@live.com>
2024-02-23 18:39:47 +0000

    challenge5 complete

--- src/challenge5.js
+++ src/challenge5.js
@@ -1,5 +1,15 @@
 
-//5. 
+//5. Use class syntax to create a Student class that initializes with a name property and has a method that returns the capitalised name.
 
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+    capitalisedName() {
+        return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+    }
+}
 
+let student = new Student("asia")
+student.capitalisedName() // "Asia"
 
