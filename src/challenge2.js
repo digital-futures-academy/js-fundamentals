c8849067d88dc85c3a995495b438f3c561653d6e
JaviRute <jrute@live.com>
2024-02-23 18:00:23 +0000

    challenge1 complete

---
+++ src/challenge2.js
@@ -0,0 +1,4 @@
+
+//2. Declare a `function` called `greet` that has 1 argument called `name` and returns a String`"Hi, <name>!"`
+
+
---
+++ src/challenge3.js
@@ -0,0 +1,3 @@
+//3. Same as Challenge 2 - this time use an arrow function
+
+
