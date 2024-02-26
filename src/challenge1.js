556badc47170099440435ab7e4e3932e6da01766
rita-s <rita_s@mail.com>
2024-02-26 12:48:47 +0000

    challenge 1

--- src/challenge1.js
+++ src/challenge1.js
@@ -1,8 +1,7 @@
 // Using the function keyword declare a function called `greet`
 // that returns a String "Hi, Ed!"
-
-function greet(name = "Ed") {
-    return `Hi, ${name}!`;
+function greet() {
+    return "Hi, Ed!";
 }
 
 console.log(greet());
