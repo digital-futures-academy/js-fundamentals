4bdcc191dcb05038396a99b3d0d026579ff38b85
Desiree <desireealexia19@gmail.com>
2024-02-29 17:51:07 +0000

    digital futures challeneges 1-6

---
+++ src/challenge3.js
@@ -0,0 +1,7 @@
+// Same as Challenge 2 - this time use an arrow function
+const greet = (name) => 'Hi, ' + name + '!';
+
+const name = "Desiree"
+const message = greet(name)
+
+console.log(message);
