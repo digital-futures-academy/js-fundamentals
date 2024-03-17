93fed025094cedc08d706201ae3f94b7771f5129
darvatu <daniel.arvatu@gmail.com>
2024-03-17 10:41:20 +0000

    challenge2

---
+++ src/challende2.js
@@ -0,0 +1,6 @@
+// Declare a `function` called `greet` that has 1 argument called `name` and returns a String `"Hi, <name>!"`
+function greet(name) {
+  return `Hi, ${name}!`;
+}
+console.log(greet("Bo"));
+console.log(greet("Dan"));
