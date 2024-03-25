053f32405f0b47371b4e1c13e13f3457e1b90976
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-03-25 17:13:57 +0000

    challenge8 success! challenge 9 begin.

--- src/challenge8.js
+++ src/challenge8.js
@@ -19,12 +19,15 @@ class LetterNumber {
         const byTwos = this.splitStringIntoPairs(encryption);
         let decryption = '';
         for (let i = 0; i < byTwos.length; ++i) {
-            decryption += this.characters[Math.abs(byTwos[i] - modulo) - 1];
+            decryption += this.characters[(100 + (byTwos[i] - modulo - 1)) % 100];
         }
         return decryption;
     }
 };
 const letterNumber = new LetterNumber();
-console.log(letterNumber.encrypt("Hi, Ed!", 0));
-console.log(letterNumber.splitStringIntoPairs('0681497203762572077292808186837291878577878677459172878672928772928081917275818880779025'))
 console.log(letterNumber.decrypt('0681497203762572077292808186837291878577878677459172878672928772928081917275818880779025', 4771));
+console.log(letterNumber.decrypt('21774590777279878686737286777776729287727587857772938872958192807273728677957287867750', 4771));
+console.log(letterNumber.decrypt('0772928081868372077280739477728293919272928077729280818679507299728477929277903584779292779072919374919281929392818786727581888077902572107792459172768772819225', 4771));
+let plaintext = "hello, world";
+let key = 5;
+console.log(plaintext === letterNumber.decrypt(letterNumber.encrypt(plaintext, key), key)); // returns true
--- src/challenge9.js
+++ src/challenge9.js
@@ -1,14 +1,33 @@
-const encryption = '35107801320554';
-function splitStringWithoutRepeating(inputString) {
-    const usedChars = new Set();
-    const outputArray = [];
-    for (let i = 0; i < inputString.length - 1; i += 2) {
-        const currentPair = inputString.slice(i, i + 2);
-        if (!usedChars.has(currentPair)) {
-            outputArray.push(currentPair);
-            usedChars.add(currentPair);
-        }
+class LetterNumberCipher {
+    characters = ` abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*()-_=+[]{};:\'"\\|,.<>/?\`~§±1234567890`;
+    twoDigit(number) {
+        const modulo = number % 100;
+        return modulo.toString().padStart(2, '0');
     }
-    return outputArray;
-}
-console.log(splitStringWithoutRepeating(encryption))
+    encrypt(str, n) {
+        let encryptedStr = '';
+        for (let i = 0; i < str.length; ++i) {
+            encryptedStr += this.twoDigit(this.characters.indexOf(str[i]) + 1 + n);
+        }
+        return encryptedStr;
+    }
+    splitStringIntoPairs(inputString) {
+        return inputString.toString().match(/.{1,2}/g);
+    }
+    decrypt(encryption, key) {
+        const modulo = key % 100;
+        const byTwos = this.splitStringIntoPairs(encryption);
+        let decryption = '';
+        for (let i = 0; i < byTwos.length; ++i) {
+            decryption += this.characters[(100 + (byTwos[i] - modulo - 1)) % 100];
+        }
+        return decryption;
+    }
+};
+const letterNumberCipher = new LetterNumberCipher();
+let plaintext = 'Look over there!'
+let key = 31045
+
+// Letter Number test cases
+console.log(letterNumberCipher.encrypt(plaintext, key)) // "84616157466168516446665451645199"
+console.log(letterNumberCipher.decrypt("84616157466168516446665451645199", key)) // 'Look over there!'
